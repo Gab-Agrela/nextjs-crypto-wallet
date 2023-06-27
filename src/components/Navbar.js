c81a4840348374752a4165d76d39b57ffc7c3fec
Agrela <gabriel.agrela@hotmail.com>
2023-06-27 18:28:55 -0300

    feat: Create project and developed Navbar and Footer

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "../styles/Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.container}>
+      <div className={styles.logo}>
+        <Image src="/images/doge.png" width={50} height={50} alt="DogeCoin" />
+        <h1>CryptoWallet</h1>
+      </div>
+      <ul className={styles.links}>
+        <Link href="/" legacyBehavior>
+          <a>Moedas</a>
+        </Link>
+        <Link href="/wallet" legacyBehavior>
+          <a>Carteira</a>
+        </Link>
+        <Link href="/about" legacyBehavior>
+          <a>Sobre</a>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
